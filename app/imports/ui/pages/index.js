d2aa9683c708eda515a10aa8b0b155c2ad09bcbd
Neel Kotak <neelk@hawaii.edu>
2017-11-12 12:46:14 -1000

    Added filler home page

--- app/imports/ui/pages/index.js
+++ app/imports/ui/pages/index.js
@@ -1,2 +1,4 @@
 import './leaderboard-page.html';
-import './leaderboard-page.js';
+// import './leaderboard-page.js'; ISSUE: WHEN I IMPORT EMPTY .JS
+import './Home-Page-Filler.html';
+// import './Home-Page-Filler';
