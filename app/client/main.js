0a9d99a999edd374279a63c7442906d0607597d4
zach2heth <zach2heth@gmail.com>
2017-11-23 22:12:39 -1000

    moves imports statments so the rest will still work

--- app/client/main.js
+++ app/client/main.js
@@ -9,15 +9,15 @@ import '/imports/ui/layouts/landing';
 import '/imports/ui/layouts/shared';
 import '/imports/ui/layouts/user';
 import '/imports/ui/layouts/gametypes';
-// import '/imports/ui/layouts/profile';
 
 import '/imports/ui/pages/directory';
 import '/imports/ui/pages/filter';
 import '/imports/ui/pages/landing';
 import '/imports/ui/pages/user';
 import '/imports/ui/pages/gametypes';
-// import '/imports/ui/pages/profile';
 
+import '/imports/ui/layouts/profile';
+import '/imports/ui/pages/profile';
 
 import '/imports/ui/stylesheets/style.css';
 import '/imports/api/base';
